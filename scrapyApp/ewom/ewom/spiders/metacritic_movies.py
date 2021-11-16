dfea6baffa471951167b0efaae11bbeecf4f2686
krojoc10 <kropf.jonas@gmail.com>
2021-11-16 13:09:37 +0100

    Added productUrlSegment to all levels and fixed all occuring errors

--- scrapyApp/ewom/ewom/spiders/metacritic_movies.py
+++ scrapyApp/ewom/ewom/spiders/metacritic_movies.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 class MovieReviewsSpider(scrapy.Spider):
     name = "movieReviews"
@@ -23,21 +24,23 @@ class MovieReviewsSpider(scrapy.Spider):
 
     # crawl movie pages and collect data
     def parse_movie_details(self, response):
-        Name = response.css('div.product_page_title > h1::text').get()
-        Type = 'Movie'
-        Metascore = response.css('div.ms_wrapper > table > tr > td.summary_right > a > span::text').get()
-        Userscore = response.css('div.user_score_summary > table > tr > td.summary_right > a > span::text').get()
-        Producer = response.css('span.distributor > a::text').get()
-        Summary = response.css('div.summary_deck span > span::text').get()
+        productName = response.css('div.product_page_title > h1::text').get()
+        type = 'Movie'
+        metascore = response.css('div.ms_wrapper > table > tr > td.summary_right > a > span::text').get()
+        userscore = response.css('div.user_score_summary > table > tr > td.summary_right > a > span::text').get()
+        producer = response.css('span.distributor > a::text').get()
+        summary = response.css('div.summary_deck span > span::text').get()
+        productUrlSegment = re.findall('movie\/(.+)', response.request.url)[0]
 
         #create dictionary with movie data
         movieData = {
-            'name': Name,
-            'type': Type,
-            'metascore': Metascore,
-            'userscore': Userscore,
-            'producer': Producer,
-            'summary': Summary
+            'productName': productName,
+            'type': type,
+            'metascore': metascore,
+            'userscore': userscore,
+            'producer': producer,
+            'summary': summary,
+            'productUrlSegment': productUrlSegment
         }
 
         #yield movieData
@@ -45,54 +48,62 @@ class MovieReviewsSpider(scrapy.Spider):
         #get link to critic and user reviews page
         reviewPageUrls = response.css('div.reviews > div > div > a.see_all::attr(href)').extract()
         criticReviewPageUrl = reviewPageUrls[0]
-        userReviewPageUrl = reviewPageUrls[1]
 
         #crawl to critic review page
         yield scrapy.Request(url = response.urljoin(criticReviewPageUrl), callback=self.parse_movie_critic_reviews)
 
-        #crawl to user review page
-        yield scrapy.Request(url = response.urljoin(userReviewPageUrl), callback=self.parse_movie_user_reviews)
+        try:
+            userReviewPageUrl = reviewPageUrls[1]
+            #crawl to user review page
+            yield scrapy.Request(url = response.urljoin(userReviewPageUrl), callback=self.parse_movie_user_reviews)
+        except: print(productUrlSegment + " has no user reviews")
     
     #crawl critic review page and collect review data
     def parse_movie_critic_reviews(self, response):
         for review in response.css('div.review'):
-            Type = 'critic'
+            type = 'critic'
             if review.css('span.author > a::text').get():
-                Author = review.css('span.author > a::text').get()
-            else: Author = review.css('span.author::text').get()
-            Score = review.css('div.metascore_w::text').get()
-            Summary = review.css('div.summary > a::text').get()
+                author = review.css('span.author > a::text').get()
+            else: author = review.css('span.author::text').get()
+            score = review.css('div.metascore_w::text').get()
+            if review.css('div.summary > a::text').get():
+                summary = review.css('div.summary > a::text').get()
+            else: summary = review.css('div.summary::text').get()
+            productUrlSegment = re.findall('movie\/(.+)\/', response.request.url)[0]
 
             #create dictionary with critic review data
             criticReviewData = {
-                'type': Type,
-                'author': Author,
-                'score': Score,
-                'summary': Summary
+                'type': type,
+                'author': author,
+                'score': score,
+                'summary': summary,
+                'productUrlSegment': productUrlSegment
             }
 
-            #yield criticReviewData
+            yield criticReviewData
 
     #crawl user review page and collect review data
     def parse_movie_user_reviews(self, response):
         for review in response.css('div.review'):
-            Type = 'user'
-            Author = review.css('span.author > a::text').get()
-            DateCreated = review.css('span.date::text').get()
-            Score = review.css('div.metascore_w::text').get()
+            type = 'user'
+            if review.css('span.author > a::text').get():
+                author = review.css('span.author > a::text').get()
+            else: author = review.css('span.author::text').get()
+            dateCreated = review.css('span.date::text').get()
+            score = review.css('div.metascore_w::text').get()
             if review.css('div.summary > div > span::text').get() is not ' ':
-                Summary = ' '.join(review.css('div.summary > div > span::text').extract())
-            else: Summary = ' '.join(review.css('div.summary > div > span > span.blurb_expanded::text').extract())
+                summary = ' '.join(review.css('div.summary > div > span::text').extract())
+            else: summary = ' '.join(review.css('div.summary > div > span > span.blurb_expanded::text').extract())
+            productUrlSegment = re.findall('movie\/(.+)\/', response.request.url)[0]
 
             #create dictionary with user review data
             userReviewData = {
-                'type': Type,
-                'author': Author,
-                'dateCreated': DateCreated,
-                'score': Score,
-                'summary': Summary
+                'type': type,
+                'author': author,
+                'dateCreated': dateCreated,
+                'score': score,
+                'summary': summary,
+                'productUrlSegment': productUrlSegment
             }
 
-            yield userReviewData
-
-#self = MovieReviewsSpider()
+            #yield userReviewData
