308f6152bf4fab47b56ce769519259e4da18b10c
krojoc10 <kropf.jonas@gmail.com>
2022-01-10 19:02:34 +0100

    adapted vgChartz for platform and releaseDate

--- scrapyApp/ewom/ewom/spiders/vgChartz_game-sales.py
+++ scrapyApp/ewom/ewom/spiders/vgChartz_game-sales.py
@@ -2,7 +2,7 @@ import scrapy
 
 class GameSalesSpider(scrapy.Spider):
     name = "gameSales"
-    start_urls = ['https://www.vgchartz.com/games/games.php?name=&keyword=&console=&region=All&developer=&publisher=&goty_year=&genre=&boxart=Both&banner=Both&ownership=Both&showmultiplat=No&results=200&order=Sales&showtotalsales=0&showtotalsales=1&showpublisher=0&showpublisher=1&showvgchartzscore=0&shownasales=0&showdeveloper=0&showcriticscore=0&showpalsales=0&showreleasedate=0&showuserscore=0&showjapansales=0&showlastupdate=0&showothersales=0&showshipped=0']
+    start_urls = ['https://www.vgchartz.com/games/games.php?name=&keyword=&console=&region=All&developer=&publisher=&goty_year=&genre=&boxart=Both&banner=Both&ownership=Both&showmultiplat=No&results=200&order=Sales&showtotalsales=0&showtotalsales=1&showpublisher=0&showpublisher=1&showvgchartzscore=0&shownasales=0&showdeveloper=0&showcriticscore=0&showpalsales=0&showreleasedate=0&showreleasedate=1&showuserscore=0&showjapansales=0&showlastupdate=0&showothersales=0&showshipped=0']
 
     #set pagecount limit
     '''custom_settings = {
@@ -15,13 +15,15 @@ class GameSalesSpider(scrapy.Spider):
         #collect title, publisher and sales data
         for dataRecord in response.css('div#generalBody > table > tr')[3:]:
             gameName = dataRecord.css(':nth-child(3) > a::text').get()
-            publisher = dataRecord.css(':nth-child(5)::text').get()
+            platform = dataRecord.css(':nth-child(4) > img::attr(alt)').get()
+            releaseDate = dataRecord.css(':nth-child(7)::text').get()
             sales = dataRecord.css(':nth-child(6)::text').get()
 
             #yield data record
             yield {
                 'gameName': gameName,
-                'publisher': publisher,
+                'platform': platform,
+                'releaseDate': releaseDate,
                 'sales': sales
             }
         
