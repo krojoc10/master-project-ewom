08c4add2bdfcb760398e849a96445c29cd5935e8
krojoc10 <kropf.jonas@gmail.com>
2022-01-19 09:58:11 +0100

    added dropping of existing collection

--- mongoDBApp/database_connection.py
+++ mongoDBApp/database_connection.py
@@ -1,7 +1,12 @@
 from pymongo import MongoClient
 
 def connect_database():
-    #connect to 
+    #connect to database
     client = MongoClient('localhost', 27017)
-    db = client.test
+    db = client['test']
+
+    #drop collection if existent
+    col = db['product_reviews']
+    col.drop()
+
     return db
