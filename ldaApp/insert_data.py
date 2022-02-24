cd86505de7c72e042e4188afcd92cb78153d963d
krojoc10 <kropf.jonas@gmail.com>
2022-02-24 11:11:19 +0100

    added document topic contribution extraction and db import

---
+++ ldaApp/insert_data.py
@@ -0,0 +1,12 @@
+def insert_data(data, db):
+    #split data into critic and user reviews
+    criticReviews = data[data['reviewType'] == 'critic']
+    userReviews = data[data['reviewType'] == 'user']
+    #insert data into database
+    criticReviews = criticReviews.to_dict('records')
+    for row in criticReviews:
+        db.product_reviews.update_one({'_id': row['_id'], 'criticReviews': {'$elemMatch': { 'author': row['author']}}}, {'$set': {'criticReviews.$.topicContribution': row['topicContribution']}})
+    userReviews = userReviews.to_dict('records')
+    for row in userReviews:
+        db.product_reviews.update_one({'_id': row['_id'], 'userReviews': {'$elemMatch': { 'author': row['author']}}}, {'$set': {'userReviews.$.topicContribution': row['topicContribution']}})
+     
--- ldaApp/topic_modelling.py
+++ ldaApp/topic_modelling.py
@@ -1,4 +1,5 @@
 from database_connection import connect_database, load_data_from_db
+from insert_data import insert_data
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import gensim
@@ -39,33 +40,77 @@ corpus = [id2word.doc2bow(review) for review in reviews]
 print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': dictionary and corpus created')
 
 #evaluate models based on different number of topics
-print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': evaluate models')
-def model_evaluation(k):
-    #build model
-    lda_model = gensim.models.LdaModel(corpus=corpus,
-                                    id2word=id2word,
-                                    num_topics=k,
-                                    chunksize=10000,
-                                    passes=1,
-                                    per_word_topics=True)
-    #compute coherence score
-    coherence_model_lda = CoherenceModel(model=lda_model, corpus=corpus, coherence='u_mass')
-    return coherence_model_lda.get_coherence()
+while True:
+    evalTrue = input('Should models with different number of topics be evaluated? (y/n):')
+    if evalTrue in ['y', 'n']:
+        break
+    else:
+        print('Please enter y or n.')
+        continue
 
-min_topics = 205
-max_topics = 255
-step_size = 5
-topics_range = range(min_topics, max_topics, step_size)
+if evalTrue == 'y':
+    print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': evaluate models')
+    def model_evaluation(k):
+        #build model
+        lda_model = gensim.models.LdaModel(corpus=corpus,
+                                        id2word=id2word,
+                                        num_topics=k,
+                                        chunksize=10000,
+                                        passes=1,
+                                        per_word_topics=True)
+        #compute coherence score
+        coherence_model_lda = CoherenceModel(model=lda_model, corpus=corpus, coherence='u_mass')
+        return coherence_model_lda.get_coherence()
 
-modelResults = {}
+    min_topics = 5
+    max_topics = 405
+    step_size = 5
+    topics_range = range(min_topics, max_topics, step_size)
 
-for k in topics_range:
-    c = model_evaluation(k)
-    modelResults.update({k:c})
-print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': models evaluated')
+    modelResults = {}
 
-#print and plot results
-print(modelResults)
-x, y = zip(*modelResults.items())
-plt.plot(x, y)
-plt.show()
+    for k in topics_range:
+        c = model_evaluation(k)
+        modelResults.update({k:c})
+    print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': models evaluated')
+
+    #print and plot results
+    print(modelResults)
+    x, y = zip(*modelResults.items())
+    plt.plot(x, y)
+    plt.xlabel('number of topics (k)')
+    plt.ylabel('coherence (u_mass)')
+    plt.show()
+
+#build model with suitable number of topics
+while True:
+    try:
+        k = int(input('Insert suitable number of topics:'))
+        break
+    except ValueError:
+        print('Please enter integer value.')
+        continue
+
+print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': build best model')
+lda_model = gensim.models.LdaModel(corpus=corpus,
+                                   id2word=id2word,
+                                   num_topics=k,
+                                   chunksize=10000,
+                                   passes=1,
+                                   per_word_topics=True)
+print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': model built')
+
+print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': extract document topic contribution')
+#save topic contribution in list
+topicContribution = []
+for doc in corpus:
+    topicContribution.append({str(key): float(value) for key, value in dict(lda_model.get_document_topics(bow=doc)).items()})
+
+#add topic contribution list to data
+data['topicContribution'] = topicContribution
+print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': document topic contribution added to dataframe')
+
+#insert sentiment score into database
+print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': insert document topic contribution into database')
+insert_data(data, db)
+print(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ': database updated')
